dad5daad4f8d1d662b6563ad1e7a474e17dd35fb
Taraka <revutaraka9704@gmail.com>
2025-12-20 19:03:05 +0530

    Version 4: safe input handling, file-based storage, and robust parsing

--- version1.cpp
+++ version1.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 #include <string>
 using namespace std;
+
+int intInput(const string& msg){
+    while (true) {
+        cout << msg;
+        string input;
+        getline(cin, input);
+        try {
+            return stoi(input);
+        } catch (...) {
+            cout << "Invalid number. Try again.\n";
+        }
+    }
+
+}
+
+string str(const string& msg){
+    cout << msg;
+    string input;
+    getline(cin,input);
+    return input;
+}
 
 class Book {
     public:
@@ -13,82 +34,100 @@ class Book {
     };
 
 
-void   search(vector<Book>&books) {
-    int ID ;
-    cout << " Enter the book ID to search :-";
-    cin >> ID;
-    cin.ignore();
-    for(auto it: books){
-        if( it.ID == ID) {
-            cout << it.ID << "|" 
-            << it.Title << " " 
-            << it.Author << endl;
-        }
-    }
+void   search(const string& filename) {
+    ifstream file(filename);
+    if(!file){
+        cout << "There is no file we can't do the search operation";
+        return;
+    }else{
+        string line;
+        int ID = intInput("Enter the Id to search:-");
+        while(getline(file,line)){
+            size_t pos = line.find(',');
+            if (pos == string::npos) continue;
+            int fileID = stoi(line.substr(0, pos));
+            if (fileID == ID){
+            cout << "Book found: " << line << endl;
+            return;
+            }
 
-    
-}
-void searcht(vector<Book>&books) {
-    string title;
-    cout << "Enter the title to check the book :-";
-    getline(cin,title);
-    for(auto it: books){
-        if( it.Title == title) {
-            cout << it.ID << "|" 
-            << it.Title << " " 
-            << it.Author << endl;
-            break;
+
         }
-        
+        cout << "with that ID i can't find a book";
     }
     
+    
 }
-void display(vector<Book>&books){
-    for(auto it: books){
-        cout << it.ID << "|" 
-        << it.Title << " " 
-        << it.Author << endl;
+void searcht(const string& filename) {
+    
+    ifstream file(filename);
+    if(!file){
+        cout << "There is no file we can't do the search operation";
+        return;
+    }
+    else{
+        string title =   str("Enter the title to check the book :-");
+        string line;
+        while(getline(file,line)){
+            string extractedTitle = line.substr(line.find(',') + 1,
+                                    line.find(',', line.find(',') + 1) - line.find(',') - 1);
+
+            if(extractedTitle == title){
+                cout << line <<endl;
+                return;
+            }
+
+        }
+    }
+    cout << "BOOk Not found with that title" << endl;
+    
+}
+void display(const string& filename){
+    ifstream file(filename);
+    if(!file){
+        cout << "There is no file we can't do the dispaly operation";
+        return;
+    }else{
+        string line;
+        while(getline(file,line)){
+            cout << line <<endl;
+        }
+
     }
 }
-void addbooks(vector<Book>&books) {
+void addbooks(const string& filename) {
     cout << "We are adding the books so please enter the details "<<endl;
+    ofstream file(filename, ios::app);
     Book b1;
-    cout <<" Enter the book ID :-";
-    cin >> b1.ID;
-    cin.ignore();
-    cout << "Enter the book Title :-";
-    getline(cin,b1.Title);
-    cout << "Enter the book Author :-";
-    getline(cin,b1.Author);
-    cout <<" Enter the book year :-";
-    cin >> b1.year;
-    cin.ignore();
-    books.push_back(b1);
+    b1.ID = intInput(" Enter the book ID :-");
+    b1.Title = str( "Enter the book Title :-");
+    b1.Author = str( "Enter the book Author :-");
+    b1.year = intInput("Enter the book year :-");
+    file << b1.ID <<","
+        << b1.Title <<","
+        << b1.Author <<","
+        << b1.year <<endl;
 
 }
 
 int main (){
-    vector<Book> books;
     string input;
     while(true) {
-        cout << " We are in loop now so u can do the tasks"<<endl;
-        int num;
-        cout << "Enter the case for the switch :-" << endl;
-        cin>>num;
-        cin.ignore();
+        cout << "\n1. Add Book\n2. Display Books\n3. Search by ID\n4. Search by Title\n5. Exit\n";
+        int num = intInput("Enter the case for the switch :-");
         switch (num)
         {
         case 1:
-            addbooks(books);
+            addbooks("library.txt");
             break;
         case 2:
-            display(books);
+            display("library.txt");
             break;
         case 3:
-            search(books);
+            search("library.txt");
             break;
         case 4:
-            searcht(books);
+            searcht("library.txt");
             break;
         case 5:
             cout << "Exiting program\n";
